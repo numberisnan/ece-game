041ba0a5f3af93ff84a41721a37465d3ee9a11fe
Faraz <farazmalik17465@gmail.com>
2024-11-20 12:49:19 -0500

    Files

---
+++ quiz.js
@@ -0,0 +1,62 @@
+const quiz = [
+    {
+        q: "Which of the following best defines preterm birth?",
+        c: 0,
+        a: [
+            "Birth occuring before 37 weeks of gestation",
+            "Birth occuring between 37 and 47 weeks of gestation",
+            "Birth occuring after 42 weeks of gestation",
+            "Birth occuring exactly at 40 weeks of gestation"
+        ]
+    },
+    {
+        q: "Which developmental domain is commonly affected by preterm birth and might require targeted support?",
+        a: [
+            "Memory and literacy",
+            "Cognitive, motor, language, and socio-emotional",
+            "Physial growth and height",
+            "General health and well-being"
+        ],
+        c: 1
+    },
+    {
+        q: "Which of the following is a common risk factor associated with preterm birth?",
+        a: [
+            "Living in rural areas",
+            "Genetic, medical, and socioeconomic factors",
+            "Being a first time parent",
+            "Higher levels of maternal education"
+        ],
+        c: 1
+    },
+    {
+        q: "What is one common reason why early intervention is essential for children born preterm?",
+        a: [
+            "It provides temporary solutions for developmental delays",
+            "It allows early detection and supports timely developmental support to improve outcomes",
+            "It is primarily use for diagnosing genetic disorders",
+            "It is only nessecary if severe delays are evident"
+        ],
+        c: 1
+    },
+    {
+        q: "Who can refer a child to the preterm services program?",
+        a: [
+            "Only physicians",
+            "Parents and caregivers",
+            "Any community member with parental consent",
+            "Teachers only"
+        ],
+        c: 2
+    },
+    {
+        q: "Up to what age are children eligable to services under the preterm program?",
+        a: [
+            "Until 3 years old",
+            "Until school entry",
+            "Until 6 years old",
+            "Until kindergarten"
+        ],
+        c: 1
+    }
+]
